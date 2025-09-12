209858057b987d8ae9734e0334b2d2158f1c2874
Josh Fester <joshfester@gmail.com>
2025-09-12 15:28:32 +0000

    revert dist changes

--- dist/optimizers/process-script.d.ts
+++
@@ -1,3 +0,0 @@
-import * as cheerio from '@divriots/cheerio';
-import type { GlobalState } from '../state.js';
-export declare function processScript(state: GlobalState, _htmlfile: string, script: cheerio.Cheerio<cheerio.Element>, isAboveTheFold: boolean, _appendToBody: Record<string, string>): Promise<void>;
